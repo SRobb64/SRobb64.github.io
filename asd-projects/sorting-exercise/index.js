412dccb07ad267e5276f9569f6569c7b2ad3b69d
SRobb64 <srr0039956@lpssonline.com>
2024-05-08 14:55:52 +0000

    update

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -29,23 +29,32 @@ async function bubbleSort (array) {
 // TODO 3: Implement quickSort
 async function quickSort (array, left, right){
     if ((right - left) > 0){
-        var index = await partition(array, left, right)
+        var index = await partition(array, left, right);
         if (left < (index - 1)){
-            await quickSort(array, left, index - 1)
+            await quickSort(array, left, index - 1);
         } else if (index < right){
-            await quickSort(array, index, right)
+            await quickSort(array, index, right);
         }
     }
 }
 
 // TODOs 4 & 5: Implement partition
-function partition (array, left, right){
-    var pivot = 20;
+async function partition (array, left, right){
+    var pivot = array[Math.floor((right + left)/2)].value;
     while (left < right){
-        while (array[left] < pivot{ left++ }){
-            // idk
+        while (array[left].value < pivot.value){
+            left += 1;
+        }
+        while (array[right].value > pivot.value){
+            right -= 1;
+        }
+        if (left < right){
+            swap(array, left, right);
+            updateCounter(quickCounter);
+            await sleep();
         }
     }
+    return left + 1;
 }
 
 // TODO 1: Implement swap
--- asd-projects/sorting-exercise/setup.js
+++ asd-projects/sorting-exercise/setup.js
@@ -9,7 +9,7 @@
 const SLEEP_AMOUNT = 200;
 
 // set the max number of squares
-const MAX_SQUARES = 5;
+const MAX_SQUARES = 8;
 
 // set constants for pseudo-random number generation
 const SEED = 2;
